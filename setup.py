502cd4f5debef8d9e92a920c107ae487f0e2f4b7
Farid Smaï <f.smai@brgm.fr>
2020-04-15 11:40:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='razorback',
-    version='0.2.2',
+    version='0.2.2b',
     description='Robust estimation of linear response functions',
     author='Farid Smai',
     author_email='f.smai@brgm.fr',
