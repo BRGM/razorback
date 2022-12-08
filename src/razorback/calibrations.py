622f1612fbcc72b65404115915904cb56eba1a84
fsmaibrgm <f.smai@brgm.fr>
2022-12-08 16:00:35 +0100

    exposing parameters of metronix calibration function

--- src/razorback/calibrations.py
+++ src/razorback/calibrations.py
@@ -2,7 +2,7 @@
 """
 
 
-import os
+from pathlib import Path
 import re
 import numpy as np
 import scipy.interpolate
@@ -15,8 +15,12 @@ __all__ = ['metronix']
 
 METRONIX_DATA_PATH = "metronix_calibration"
 
+METRONIX_CHOPPER_ON_LIMIT = 512.
+METRONIX_VERSION_PATTERN = r".*(MFS\d\d).*"
+METRONIX_ALPHA_MAP = {'MFS06': 4.0, 'MFS07': 32.0}
 
-def metronix(filename, sampling_rate, chopper_on_limit=512.):
+
+def metronix(filename, sampling_rate, chopper_on_limit=None, version_pattern=None, alpha_map=None):
     """ return calibration function for metronix devices
 
     Parameters
@@ -27,6 +31,13 @@ def metronix(filename, sampling_rate, chopper_on_limit=512.):
     sampling_rate: float
     chopper_on_limit: float [optional]
         threshold for activating chopper
+        default to METRONIX_CHOPPER_ON_LIMIT
+    version_pattern: str [optional]
+        regexp pattern for extracting sensor version identifier from filename
+        default to METRONIX_VERSION_PATTERN
+    alpha_map: dict (version->alpha) [optional]
+        alpha value to use for sensor version
+        default to METRONIX_ALPHA_MAP
 
     Returns
     -------
@@ -40,6 +51,7 @@ def metronix(filename, sampling_rate, chopper_on_limit=512.):
         - sampling_rate >  chopper_on_limit -> chopper off
 
     """
+
     def start_stop(lines, mark):
         pattern = r'\s+'.join(re.split(r'\s+', mark.lower()))
         start = next(i for (i, l) in enumerate(lines, 1)
@@ -50,35 +62,29 @@ def metronix(filename, sampling_rate, chopper_on_limit=512.):
         return start, stop
 
     def version(filename):
-        pattern = r".*(MFS\d\d).*"
-        #
-        name, _ = os.path.splitext(os.path.basename(filename))
-        m = re.match(pattern, name)
+        name = Path(filename).stem
+        m = re.match(version_pattern, name)
         if m is None:
-            raise ValueError("cannot find version of calibration file '%s'"
-                             % filename)
+            raise ValueError(f"cannot find version of calibration file '{filename}'")
         return m.group(1)
 
     def cal_mp(freq, module, phase):
         return freq * module * np.exp(1j * phase * np.pi/180.)
 
-    def calibration(table, filename, chopper):
+    def calibration(table, filename, chopper, alpha_map):
         freq = table[:, 0]
         calib = cal_mp(freq, table[:, 1], table[:, 2])
         tabuled = scipy.interpolate.interp1d(freq, calib, copy=False)
 
         vers = version(filename)
+        if vers not in alpha_map:
+            raise ValueError(
+                f"unknown version '{vers}' of calibration file '{filename.name}'"
+            )
+
+        alpha = alpha_map[vers]
         freq_min = freq[0]
         mod_min = table[0, 1]
-        alpha = {
-            'MFS06': 4.0,
-            'MFS07': 32.0,
-        }.get(vers, None)
-        if alpha is None:
-            raise ValueError(
-                "unknown version %r of calibration file %r"
-                % (vers, filename)
-            )
 
         def calib_func(f):
             if f < freq_min and chopper:
@@ -88,6 +94,13 @@ def metronix(filename, sampling_rate, chopper_on_limit=512.):
 
         return calib_func
 
+    if chopper_on_limit is None:
+        chopper_on_limit = METRONIX_CHOPPER_ON_LIMIT
+    if version_pattern is None:
+        version_pattern = METRONIX_VERSION_PATTERN
+    if alpha_map is None:
+        alpha_map = METRONIX_ALPHA_MAP
+
     filename = get_data_file(filename, METRONIX_DATA_PATH)
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -95,7 +108,7 @@ def metronix(filename, sampling_rate, chopper_on_limit=512.):
     chopper = sampling_rate <= chopper_on_limit
     mark = 'Chopper On' if chopper else 'Chopper Off'
     start, stop = start_stop(lines, mark)
-    calib = calibration(np.loadtxt(lines[start:stop]), filename, chopper)
+    calib = calibration(np.loadtxt(lines[start:stop]), filename, chopper, alpha_map)
     return calib
 
 
