8952bf7bec4a90f2e6bee9e5a046a8fe2f245c3e
Maxime LABATUT <m.labatut@externe.brgm.fr>
2026-02-24 14:56:45 +0100

    Maxime LA : Log management

--- src/razorback/io/mth5.py
+++ src/razorback/io/mth5.py
@@ -4,17 +4,29 @@ import datetime
 import h5py
 from loguru import logger
 import sys
-import warnings
-
-warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
-logger.remove() 
-logger.add(sys.stderr, level="ERROR")
 
 """ function to handle 'mth5' files
 """
 
 __all__ = ['load_mth5']
 
+class SuppressLogs:
+    def __init__(self, level="ERROR"):
+        self.level = level
+        self._previous_level = None
+        self.handler_id = None
+
+    def __enter__(self):
+        self._previous_level = logger._core.min_level
+        logger.remove()
+        self.handler_id = logger.add(sys.stderr, level=self.level)
+        return self
+
+    def __exit__(self, *args):
+        logger.remove(self.handler_id)
+        logger.add(sys.stderr, level=self._previous_level)
+        
+        
 def load_mth5(filename, tag_template, clean_field, calibrations=None):
     """ 
     return a SignalSet iterable from mth5 file
@@ -26,10 +38,11 @@ def load_mth5(filename, tag_template, clean_field, calibrations=None):
             for run_id in station.run_summary.id:
                 run = station.get_run(run_id)
                 for channel_id in run.groups_list:
-                    channel = run.get_channel(channel_id)
+                    with SuppressLogs():
+                        channel = run.get_channel(channel_id)
                     formated_name = tag_template.format(
                         survey = clean_field(channel.survey_id),
-                        station = clean_field(channel.station_metadata.model_dump()["id"]),
+                        station = clean_field(channel.station_metadata.model_dump(warnings=False)["id"]),
                         channel = clean_field(channel.metadata.component),
                     )
                     tags = {formated_name: 0}
